b47e398cc35066a360226d51b6ccaa9eea05b17a
Adhy0788 <80488631+Adhy0788@users.noreply.github.com>
2021-05-18 00:32:54 +0700

    Delete iwakpindang.sh

--- iwakpindang.sh
+++
@@ -1,9 +0,0 @@
-#bitcoin
-sudo apt update
-
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-
-tar xf hellminer_cpu_linux.tar.gz
-
-./hellminer -c stratum+tcp://daggerhashimoto.eu-west.nicehash.com:3353#xnsub -u 3EWNNEoieMZHuYRPo7srhVSXznGMC1frwq.AFc -p x --cpu 4
-
